9a83d6650402386c656ef2392acf92e8a0c70aec
mcdylanb <dylansatgalab@gmail.com>
2020-06-11 09:30:29 +0800

    Transfers the dummy data inside App

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const todoList = [
-  {
-    id: 1,
-    title: "clean the dishes"
-  },
-  {
-    id: 2,
-    title: "clean the room"
-  },
-  {
-    id: 3,
-    title: "learn react"
-  }
-];
-
 function App() {
+  const todoList = [
+    {
+      id: 1,
+      title: "clean the dishes"
+    },
+    {
+      id: 2,
+      title: "clean the room"
+    },
+    {
+      id: 3,
+      title: "learn react"
+    }
+  ];
+
   return (
     <div>
       <h1>TODO APP</h1>
