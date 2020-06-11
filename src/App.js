de65c24b31d7d394d66bedf1fe8b43b96e004ef3
mcdylanb <dylansatgalab@gmail.com>
2020-06-11 09:28:43 +0800

    adds a dummy data within App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+const todoList = [
+  {
+    id: 1,
+    title: "clean the dishes"
+  },
+  {
+    id: 2,
+    title: "clean the room"
+  },
+  {
+    id: 3,
+    title: "learn react"
+  }
+];
 
 function App() {
   return (
